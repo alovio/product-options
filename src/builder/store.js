3d702e9a364478f0802d598f1ea532eeb2b39fe5
Tahir <ttaxiir@gmail.com>
2026-07-05 13:05:19 +0400

    chore: remove v1 metabox builder sources + their store suite (replaced by CF port)

--- src/builder/store.js
+++
@@ -1,9 +0,0 @@
-import { createReduxStore, register } from '@wordpress/data';
-import { reducer, actions, selectors } from './reducer';
-
-export const STORE = 'apo/builder';
-
-const store = createReduxStore( STORE, { reducer, actions, selectors } );
-register( store );
-
-export default store;
